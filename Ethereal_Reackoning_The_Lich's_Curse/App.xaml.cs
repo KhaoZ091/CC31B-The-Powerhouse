5362f2e44dc89c315319981508d8ee82d90786f0
Khaoz091 <rapatalolorenz@gmail.com>
2024-03-03 19:07:10 +0800

    first commit

---
+++ Ethereal_Reackoning_The_Lich's_Curse/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Ethereal_Reackoning_The_Lich_s_Curse;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
